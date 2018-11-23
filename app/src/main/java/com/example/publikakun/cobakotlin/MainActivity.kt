9f181b884d9b5a4f43a7c5c4b2947055fff86058
Nyoman <nyoman@syde.co>
2018-11-23 11:36:01 +0700

    [nyoman] add Intent

--- app/src/main/java/com/example/publikakun/cobakotlin/MainActivity.kt
+++ app/src/main/java/com/example/publikakun/cobakotlin/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.publikakun.cobakotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.publikakun.cobakotlin.utils.Constant
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text = enterNameEdt.text
+        showNameButton.setOnClickListener {
+            if (text.isEmpty()) resultTxt.text="Enter name" else resultTxt.text="Welcome $text"
+
+        }
+        btn_intent.setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/publikakun/cobakotlin/utils/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.publikakun.cobakotlin.utils
+
+interface Constant {
+
+}
